e9ce4404214c973d2472c6c928c67faf0a6e8349
hyperion912 <abhishekgzp615@gmail.com>
2025-11-17 20:12:51 +0530

    node-realtime

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -5,6 +5,9 @@ import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
 import { HttpRequestFormValues, HttpRequestDialog } from "./diaglog";
+import { useNodeStatus } from "../../hooks/use-node-status";
+import { fetchHttpReaquestRealtimeToken } from "./actions";
+import { HTTP_REQUEST_CHANNEL_NAME } from "@/inngest/channels/http-request";
 
 type HttpRequestNodeData = {
     variableName?: string;
@@ -18,7 +21,12 @@ type HttpRequestNodeType = Node<HttpRequestNodeData>;
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
     const [dialogOpen, setDialogOpen] = useState(false);
     const {setNodes} = useReactFlow()
-    const nodeStatus = "initial";
+    const nodeStatus = useNodeStatus({
+        nodeId: props.id,
+        channel: HTTP_REQUEST_CHANNEL_NAME,
+        topic: "status",
+        refreshToken: fetchHttpReaquestRealtimeToken,
+    });
     const handleOpenSettings = () => {
         setDialogOpen(true);
     }
